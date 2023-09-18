8951beb38f7b085ac91f41991d4016d63a380f7f
caiomelotec <caio.melotec@icloud.com>
2023-09-18 20:53:51 +0200

    who

---
+++ src/components/Who.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Section = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.section`
+  height: 100%;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.section`
+  flex: 1;
+`;
+const Title = styled.h1`
+  font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 60px;
+  }
+`;
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: white;
+`;
+
+const Desc = styled.p`
+  font-size: 30px;
+  background-color: rgb(3, 107, 243);
+  color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
+const Button = styled.button`
+  background-color: rgb(3, 107, 243);
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.section`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  @media only screen and (max-width: 768px) {
+    align-items: center;
+    text-align: center;
+  }
+`;
+export const Who = () => {
+  return (
+    <Section>
+      <Container>
+        <Left>{/*3D model */}</Left>
+        <Right>
+          <Title>Think outside the square space</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>Who we Are</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            a creative group of designers and developers with a passion for the
+            arts.
+          </Desc>
+          <Button>See our works</Button>
+        </Right>
+      </Container>
+    </Section>
+  );
+};
