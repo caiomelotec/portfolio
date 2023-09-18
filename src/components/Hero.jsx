877fa5f7d9088d7e444f1a98ac4df57a51ada422
caiomelotec <caio.melotec@icloud.com>
2023-09-18 20:54:40 +0200

    Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+
+import styled from "styled-components";
+import { NavBar } from "./NavBar";
+
+const Section = styled.section`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.section`
+  height: 100%;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.section`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+const Title = styled.h1`
+  font-size: 74px;
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+  }
+`;
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: white;
+`;
+
+const Desc = styled.p`
+  font-size: 30px;
+  background-color: rgb(3, 107, 243);
+  color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
+const Button = styled.button`
+  background-color: rgb(3, 107, 243);
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.section`
+  flex: 3;
+  position: relative;
+`;
+const Img = styled.img`
+  width: 600px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: rotate 10s linear infinite;
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+export const Hero = () => {
+  return (
+    <Section>
+      <NavBar />
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png" />
+            <Subtitle>What we Do</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            we enjoy creating delightful, human-centered digital experiences.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          {/*3D model */}
+          <Img src="./img/moon02.png" className="img" />
+        </Right>
+      </Container>
+    </Section>
+  );
+};
